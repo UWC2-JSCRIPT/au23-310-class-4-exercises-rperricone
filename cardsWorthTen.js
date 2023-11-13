b6d5e83dc417311bbab50bb3c8861cff7a0caf00
Robin <robinp31@uw.eduL>
2023-11-12 21:35:34 -0700

    fixed comma issue

--- cardsWorthTen.js
+++ cardsWorthTen.js
@@ -26,5 +26,5 @@ const cardsWorthTen = cards => {
   //animals.filter((animal) => animal.kind === 'reptile');
 };
 
-console.log(cardsWorthTen(cards));
+console.log(cardsWorthTen(cards).join(","));
 // should return/log "10, Jack, Queen, King"
